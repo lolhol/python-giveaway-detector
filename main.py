b344b549966ab4c34a33e66f52e3b879c1e33453
brigerodev <62488289+lolhol@users.noreply.github.com>
2024-05-20 00:30:37 -0700

    add comments

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
+# how this works...
+# basically, it takes a screenshot of the full screen every 60 sec, parses all the text from it,
+# if the text that you want is there it will call the function that you can customize
+# works on mac, windows and prob linux (im on mac so i cant test windows or linux)
+
 import os
 import pytesseract
 from PIL import Image
 import time
 
+# the text that will trigger the bot
+textToDetect = "Flash Giveaway"
+
+# the less you put, the more accurate the bot will be but the performance will suffer
+secBetweenChecks = 60
 
 # insert your code here to actually let you know that the text has been detected
 def foundTextInImg():
@@ -10,7 +20,6 @@ def foundTextInImg():
 
 
 # this is a simple script that will detect whatever text you want on the screen
-textToDetect = "Flash Giveaway"
 while True:
     os.system("screencapture screen.png")
     image = Image.open('screen.png')
@@ -19,4 +28,4 @@ while True:
         foundTextInImg()
         break
     os.remove("screen.txt")
-    time.sleep(60)
+    time.sleep(secBetweenChecks)
